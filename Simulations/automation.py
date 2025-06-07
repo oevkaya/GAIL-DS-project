24686be5d5cb04e1c6099c52e4767988321ce2da
niwq15 <s1792412@ed.ac.uk>
2025-06-07 16:21:24 +0100

    update responses for UK-visitor-numbers

--- Simulations/automation.py
+++ Simulations/automation.py
@@ -10,9 +10,9 @@ from pprint import pprint
 
 N = 50 # number of rounds
 
-data = 'laptop_data_cleaned.csv'
-file_id = 'file-2JxoJLhwmf8KZfypqywhw3'
-folder_path = 'Simulations/output/laptop'
+data = 'UK-visitor-numbers.csv'
+file_id = 'file-LB38GMvxj7SfRMPrenaBR1'
+folder_path = 'Simulations/output/UK-visitor-numbers'
 
 
 with open(f"{folder_path}/input.json", "r", encoding="utf-8") as f:
@@ -22,19 +22,7 @@ content_set = input['content_set']
 concept_set = input['concept_set']
 
 Q_num = len(concept_set)
-
-# content_set = [
-#     'Give me the statistical summary for the given data set',
-#     'Create the suitable data visualization to summarize the price distribution and its relationship with other features',
-#     'Implement the linear regression for the price variable for the given data set',
-#     'Test and Interpret the performance of the fitted regression model based on the model diagnostic plots',
-#     """Implement different regression models as the alternative of fitted linear regression model for the price variable for the given data set. 
-# Compare the performance of the fitted models""",
-#     """Fit a regression tree model for the price variable for the given data set and test its performance. 
-# Tune the model hyperparameters by using the cross-validation approach""",
-#     """Consider the classification problem and use models like logistic regression, decision trees, or neural networks 
-# for laptops into different price ranges (e.g., budget, mid-range, premium)"""
-#     ]
+Q_remain = [i for i in range(Q_num) if i not in {0, 1, 5, 7}]
 
 openai_key="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"
 
@@ -60,24 +48,29 @@ assistant = client.beta.assistants.create (
     }
 )
 
+assistant_id = assistant.id
 print(assistant)
 
 # run multi-round simulations
-for Q in range(Q_num):
+for Q in Q_remain:
     # create an empty text file
     text_file = f"{folder_path}/Q{Q}.txt"
     with open(text_file, "w") as f:
       pass
+    
 
     # create an empty image folder if necessary
-    if concept_set[Q] == 'Data Visualization':
-        image_folder = f"{folder_path}/Q{Q}_image"
-        os.makedirs(image_folder, exist_ok=True)
-        print(f"Folder '{image_folder}' created (or already exists).")
+    # if concept_set[Q] == 'Data Visualization':
+    image_folder = f"{folder_path}/Q{Q}_image"
+    os.makedirs(image_folder, exist_ok=True)
+    print(f"Folder '{image_folder}' created (or already exists).")
 
 
     for k in range(N):
-      print(f"Round: {k}")
+    #   print(f"Round: {k}")
+      if k % 10 == 0:
+        print(f"Round: {k}")
+
       start_time = time.time()
       #------------------Create a thread and run------------------
       thread = client.beta.threads.create()
@@ -89,18 +82,18 @@ for Q in range(Q_num):
       thread_id = thread.id
 
       run = client.beta.threads.runs.create(
-        thread_id = thread.id,
-        assistant_id = assistant.id,
+        thread_id = thread_id,
+        assistant_id = assistant_id,
       )
 
       run = client.beta.threads.runs.retrieve(
-        thread_id = thread.id,
+        thread_id = thread_id,
         run_id = run.id
       )
 
       while run.status not in ["completed", "failed"]:
         run = client.beta.threads.runs.retrieve(
-        thread_id = thread.id,
+        thread_id = thread_id,
         run_id = run.id
         )
         time.sleep(10)
@@ -109,46 +102,72 @@ for Q in range(Q_num):
       runtime = time.time() - start_time
       status = run.status
       messages = client.beta.threads.messages.list(
-        thread_id = thread.id,
+        thread_id = thread_id,
       )
     
       #------------------Result Extraction------------------
-      if concept_set[Q] == 'Data Visualization':
-          image_id = None
-          for message in reversed(list(messages)):
-            for content in message.content:
-               if content.type == 'image_file':
-                 image_id = content.image_file.file_id
-                 image_data = client.files.content(image_id)
-                 image_data_bytes = image_data.read()
-
-                 with open(f"{image_folder}/{k}.png", "wb") as file:
-                    file.write(image_data_bytes)
+      image_id = None
+      for message in reversed(list(messages)):
+        for content in message.content:
+           if content.type == 'image_file':
+            image_id = content.image_file.file_id
+            image_data = client.files.content(image_id)
+            image_data_bytes = image_data.read()
+            
+            with open(f"{image_folder}/{k}.png", "wb") as file:
+               file.write(image_data_bytes)
 
           
-          with open(text_file, "a", encoding="utf-8") as f:
-            f.write("-"*100)
-            f.write(f"\nRound {k} with thread_id: {thread.id}\n")
-            f.write(f"\nimage_id: {image_id}\n")
-            f.write(f"\nStatus: {status}, Runtime: {runtime}\n")
+      with open(text_file, "a", encoding="utf-8") as f:
+        f.write("-"*100)
+        f.write(f"\nRound {k} with thread_id: {thread.id}\n")
+        f.write(f"\nimage_id: {image_id}\n")
+        f.write(f"\nStatus: {status}, Runtime: {runtime}\n")
 
-            for message in reversed(list(messages)):
-               for content in message.content:
-                 if content.type == 'text':
-                   f.write("-"*50)
-                   f.write(f"\nRole: {message.role}\n")
-                   f.write(f"\n{content.text.value}\n\n")
+        for message in reversed(list(messages)):
+           for content in message.content:
+             if content.type == 'text':
+               f.write("-"*50)
+               f.write(f"\nRole: {message.role}\n")
+               f.write(f"\n{content.text.value}\n\n")
+    #   if concept_set[Q] == 'Data Visualization':
+    #       image_id = None
+    #       for message in reversed(list(messages)):
+    #         for content in message.content:
+    #            if content.type == 'image_file':
+    #              image_id = content.image_file.file_id
+    #              image_data = client.files.content(image_id)
+    #              image_data_bytes = image_data.read()
 
-      else:
-        with open(text_file, "a", encoding="utf-8") as f:
-          f.write("-"*100)
-          f.write(f"\nRound {k} with thread_id: {thread_id}\n")
-          f.write(f"\nStatus: {status}, Runtime: {runtime}\n")
+    #              with open(f"{image_folder}/{k}.png", "wb") as file:
+    #                 file.write(image_data_bytes)
 
-          for message in reversed(list(messages)):
-            content = message.content[0].text.value
-            f.write("-"*50)
-            f.write(f"\nRole: {message.role}\n")
-            f.write(f"\n{content}\n\n")
+          
+    #       with open(text_file, "a", encoding="utf-8") as f:
+    #         f.write("-"*100)
+    #         f.write(f"\nRound {k} with thread_id: {thread.id}\n")
+    #         f.write(f"\nimage_id: {image_id}\n")
+    #         f.write(f"\nStatus: {status}, Runtime: {runtime}\n")
+
+    #         for message in reversed(list(messages)):
+    #            for content in message.content:
+    #              if content.type == 'text':
+    #                f.write("-"*50)
+    #                f.write(f"\nRole: {message.role}\n")
+    #                f.write(f"\n{content.text.value}\n\n")
+
+    #   else:
+    #     with open(text_file, "a", encoding="utf-8") as f:
+    #       f.write("-"*100)
+    #       f.write(f"\nRound {k} with thread_id: {thread_id}\n")
+    #       f.write(f"\nStatus: {status}, Runtime: {runtime}\n")
+
+    #       for message in reversed(list(messages)):
+    #         content = message.content[0].text.value
+    #         f.write("-"*50)
+    #         f.write(f"\nRole: {message.role}\n")
+    #         f.write(f"\n{content}\n\n")
             
-    
+
+
+response = client.beta.assistants.delete(assistant.id)
